f2a52cf27b2843707561cb609655e1b231c20e87
shalaka <shalakaj23@gmail.com>
2021-11-10 00:20:31 +0530

    Phone-3 :create required packages

---
+++ src/main/java/com/BikkadIt/PhoneBookApppApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApppApplication.class, args);
+	}
+
+}
